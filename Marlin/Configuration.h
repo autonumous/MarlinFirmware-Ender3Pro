f88adec23b323bdb31737586050ac4e43bcb497b
Autonumous <paulbegley@me.com>
2021-02-06 14:39:16 +0000

    name Change

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -132,7 +132,8 @@
 #endif
 
 // Name displayed in the LCD "Ready" message and Info menu
-#define CUSTOM_MACHINE_NAME "Ender-3 Pro"
+#define CUSTOM_MACHINE_NAME "Pandora NDR3" //"Ender-3 Pro"
+  
 
 // Printer's unique ID, used by some programs to differentiate between machines.
 // Choose your own or use a service like https://www.uuidgenerator.net/version4
