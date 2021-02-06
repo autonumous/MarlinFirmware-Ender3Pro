b9dc96e794b717f56b0d1c4acc381be284b9d9b6
Autonumous <paulbegley@me.com>
2021-02-06 23:26:32 +0000

    disable some debug options

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -3731,12 +3731,12 @@
 //
 // M42 - Set pin states
 //
-#define DIRECT_PIN_CONTROL
+//#define DIRECT_PIN_CONTROL
 
 //
 // M43 - display pin status, toggle pins, watch pins, watch endstops & toggle LED, test servo probe
 //
-#define PINS_DEBUGGING
+//#define PINS_DEBUGGING
 
 // Enable Marlin dev mode which adds some special commands
-#define MARLIN_DEV_MODE
+//#define MARLIN_DEV_MODE
