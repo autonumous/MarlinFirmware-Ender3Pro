be189fedaa1e4b4672143d8bd7fbd486f4b798c6
Autonumous <paulbegley@me.com>
2021-02-13 00:19:36 +0000

    Dsiabled the Z endstop

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -635,7 +635,7 @@
 // extra connectors. Leave undefined any used for non-endstop and non-probe purposes.
 #define USE_XMIN_PLUG
 #define USE_YMIN_PLUG
-#define USE_ZMIN_PLUG
+//#define USE_ZMIN_PLUG  //Using Probe instead, Allowing  switch to be disconnected
 //#define USE_XMAX_PLUG
 //#define USE_YMAX_PLUG
 //#define USE_ZMAX_PLUG
@@ -729,7 +729,7 @@
 //#define ENDSTOP_NOISE_THRESHOLD 2
 
 // Check for stuck or disconnected endstops during homing moves.
-//#define DETECT_BROKEN_ENDSTOP
+#define DETECT_BROKEN_ENDSTOP
 
 //=============================================================================
 //============================== Movement Settings ============================
