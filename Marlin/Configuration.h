e06b97e08659ac785434d7a5b8338d587aca4d0c
Autonumous <paulbegley@me.com>
2021-02-13 00:50:38 +0000

    change speed of probe movement

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1020,7 +1020,7 @@
 #define PROBING_MARGIN 10
 
 // X and Y axis travel speed (mm/min) between probes
-#define XY_PROBE_SPEED 133*60 //10000//133*60)
+#define XY_PROBE_SPEED 10000//133*60)
 
 // Feedrate (mm/min) for the first approach when double-probing (MULTIPLE_PROBING == 2)
 #define Z_PROBE_SPEED_FAST (8*60)
