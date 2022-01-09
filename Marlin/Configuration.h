ea38655aef77010cb74d7ee092a506fa6095509f
Autonumous <paulbegley@me.com>
2022-01-09 23:57:38 +0000

    added temp settings

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1922,6 +1922,12 @@
 #define PREHEAT_2_TEMP_CHAMBER 35
 #define PREHEAT_2_FAN_SPEED   128 // Value from 0 to 255
 
+#define PREHEAT_3_LABEL       "PETG"
+#define PREHEAT_3_TEMP_HOTEND 240
+#define PREHEAT_3_TEMP_BED     70
+#define PREHEAT_3_TEMP_CHAMBER 35
+#define PREHEAT_3_FAN_SPEED   128 // Value from 0 to 255
+
 /**
  * Nozzle Park
  *
